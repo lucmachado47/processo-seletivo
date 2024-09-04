04342ff3b859e1dbc0066b9a7f71ef0cc3c69006
Lucas Machado <lucas_machado47@outlook.com>
2024-09-03 21:00:09 -0300

    atualização

--- 3.py
+++ 3.py
@@ -1,14 +1,16 @@
 import json
 
-with open ('processo-seletivo/faturamento_diario.json', 'r') as arquivo:
+with open ('dados.json', 'r') as arquivo:
     valores = json.load(arquivo)
 
 faturamento_diario = []
 
-for dia in valores['faturamento_diario']:
-    if dia['valor'] > 0:
-        faturamento_diario.append(dia['valor'])
+for valor in valores:
+    if valor['valor'] > 0:
+        faturamento_diario.append(valor['valor'])
 
+menor = min(faturamento_diario)
+maior = max(faturamento_diario)
 media = sum(faturamento_diario) / len(faturamento_diario)
 
 valor_superior_a_media = []
@@ -17,7 +19,9 @@ for faturamento in faturamento_diario:
     if faturamento > media:
         valor_superior_a_media.append(faturamento)
 
-print('O menor valor de faturamento foi: R$', min(faturamento_diario))
-print('O maior valor de faturamento foi: R$', max(faturamento_diario))
-print('O Número de dias com faturamento acima da média foi:', 
+
+print(f'O menor valor de faturamento foi: R$ {menor:.2f}')
+print(f'O maior valor de faturamento foi: R$ {maior:.2f}')
+print(f'A média de faturamento foi: {media:.2f}')
+print(f'O Número de dias com faturamento acima da média foi:', 
       len(valor_superior_a_media))
